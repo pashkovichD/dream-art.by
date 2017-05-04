e6bb8670a2c5c93b2dc6117fb2a456cc6f6ce2c4
pashkovichD <d.pashkovich@gmail.com>
2017-05-04 16:54:14 +0300

    end day 04.05.17

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,27 @@ $(function() {
 	$(".toggle-menu").click(function() {
 		$(this).toggleClass("on");
 		$(".main-menu").slideToggle();
+		$(".dropdown-phone").hide();
+	});
+
+	// $(".phone").hover(function() {		
+	// 	// $(".dropdown-phone").slideToggle();
+	// 	$(".dropdown-phone").toggle();
+	// });
+
+	$(".phone").click(function() {		
+		// $(".dropdown-phone").slideToggle();
+		$(".dropdown-phone").toggle();
+		var main_menu = $(".main-menu").css('display'); // состояние меню (видно/не видно)
+		
+		if($(".wrap-button > a").hasClass('on') && (main_menu == 'block')) {
+			$(".main-menu").slideToggle();
+			$(".toggle-menu").removeClass("on");
+		}
+	});
+
+	$(".dropdown-phone").click(function() {
+		return false;
 	});
 
 });
