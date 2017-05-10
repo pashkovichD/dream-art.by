8f8ecf64371490324f8e5a01da0aa8f06908f774
pashkovichD <d.pashkovich@gmail.com>
2017-05-10 16:54:08 +0300

    end day 10.05.17

--- app/js/common.js
+++ app/js/common.js
@@ -26,4 +26,81 @@ $(function() {
 		return false;
 	});
 
+	
+	
+	/*--- LavaLamp Menu ---*/
+
+	
+	var lavalamp = $('.main-menu > ul');
+	var lavalamp_li = $('.main-menu > ul > li');
+
+	lavalamp.prepend('<li class="lava-item"></li>');
+	var padding_val = parseInt($('.menu-active').css('padding-left'));
+	var prePosX = $('.menu-active').position().left + padding_val - 5;
+	var preWidthX = $('.menu-active').width() + 10;
+
+	lavalamp_li.not('li.lava-item').click(function() {
+		$(this).siblings().removeClass('menu-active');
+		$(this).addClass('menu-active');
+
+		padding_val = parseInt($(this).css('padding-left'));
+		prePosX = $(this).position().left + padding_val - 5;
+		preWidthX = $(this).width() + 10;
+	});
+
+	$('li.lava-item').css({
+		left: prePosX + 'px',
+		width : preWidthX + 'px'		
+	});
+
+	lavalamp_li.not('li.lava-item').hover(function() {
+		var padding_val = parseInt($(this).css('padding-left'));
+		var posX = $(this).position().left + padding_val - 5;
+		var widthX = $(this).width() + 10;
+		$('li.lava-item').css({
+			left: posX + 'px',
+			width : widthX + 'px'			
+		});
+	}, function() {
+		$('li.lava-item').css({
+			left: prePosX + 'px',
+			width : preWidthX + 'px'			
+		});
+	});
+
+	
+
+	/*$('.lavalamp').prepend('<li class="lava-item"></li>');
+	var prePosX = $('.menu-active').position().left;
+	var preWidthX = $('.menu-active').width();
+
+	$('.lavalamp > li').not('li.lava-item').click(function() {
+		$(this).siblings().removeClass('menu-active');
+		$(this).addClass('menu-active');
+
+		prePosX = $(this).position().left;
+		preWidthX = $(this).width();
+	});
+
+	$('li.lava-item').css({
+		left: prePosX + 'px',
+		width : preWidthX + 'px'
+	});
+
+	$('.lavalamp > li').not('li.lava-item').hover(function() {
+		var posX = $(this).position().left;
+		var widthX = $(this).width();
+		$('li.lava-item').css({
+			left: posX + 'px',
+			width : widthX + 'px'
+		});
+	}, function() {
+		$('li.lava-item').css({
+			left: prePosX + 'px',
+			width : preWidthX + 'px'
+		});
+	});*/
+
+	/*--- END LavaLamp Menu ---*/
+
 });
